bbd80e78a5c009e99e3832b8c03522cb8e085c50
mohammedhany4213-create <mohammedhany4213@gmail.com>
2026-08-31 18:35:00 +0300

    feat: register user dependencies and controllers

--- backend/src/coFind.Api/Program.cs
+++ backend/src/coFind.Api/Program.cs
@@ -1,22 +1,31 @@
+using coFind.Application.Interfaces;
+using coFind.Application.Services;
 using coFind.Infrastructure.Data;
+using coFind.Infrastructure.Repositories;
+using coFind.Infrastructure.Security;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
+builder.Services.AddScoped<UserService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
+app.MapControllers();
 
+app.Run();
