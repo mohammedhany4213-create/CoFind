27b88317a8bad1384e9c808190d9f7ddb3a673df
mohammedhany4213-create <mohammedhany4213@gmail.com>
2026-09-01 14:14:08 +0300

    fix: tighten registration input validation

--- backend/src/coFind.Application/DTOs/RegisterUserRequest.cs
+++ backend/src/coFind.Application/DTOs/RegisterUserRequest.cs
@@ -3,8 +3,8 @@ using System.ComponentModel.DataAnnotations;
 namespace coFind.Application.DTOs;
 
 public record RegisterUserRequest(
-    [property: Required, MinLength(2), MaxLength(100)] string Name,
+    [property: Required, MinLength(2), MaxLength(50)] string Name,
     [property: Required, EmailAddress, MaxLength(100)] string Email,
     [property: Required, MinLength(8), MaxLength(100)] string Password,
-    [property: Required, MaxLength(30)] string WhatsappNumber
+    [property: Required, Phone, MaxLength(15)] string WhatsappNumber
 );
