3b7aec4a198900de3fc1e26acee249b1a1586bb2
mohammedhany4213-create <mohammedhany4213@gmail.com>
2026-09-01 18:48:19 +0300

    feat: extend token service for refresh tokens

--- backend/src/coFind.Application/Interfaces/ITokenService.cs
+++ backend/src/coFind.Application/Interfaces/ITokenService.cs
@@ -5,4 +5,6 @@ namespace coFind.Application.Interfaces;
 public interface ITokenService
 {
     string GenerateToken(User user);
+    string GenerateRefreshToken();
+    string HashRefreshToken(string refreshToken);
 }
