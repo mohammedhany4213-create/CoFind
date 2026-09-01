7188ae6a8fc5ba8274c19dbbde2a0e4e5febee3a
mohammedhany4213-create <mohammedhany4213@gmail.com>
2026-09-01 19:22:59 +0300

    fix: apply record validation metadata to constructor parameters

--- backend/src/coFind.Application/DTOs/LoginUserRequest.cs
+++ backend/src/coFind.Application/DTOs/LoginUserRequest.cs
@@ -3,6 +3,6 @@ using System.ComponentModel.DataAnnotations;
 namespace coFind.Application.DTOs;
 
 public record LoginUserRequest(
-    [property: Required, EmailAddress, MaxLength(100)] string Email,
-    [property: Required, MinLength(8), MaxLength(100)] string Password
+    [Required, EmailAddress, MaxLength(100)] string Email,
+    [Required, MinLength(8), MaxLength(100)] string Password
 );
