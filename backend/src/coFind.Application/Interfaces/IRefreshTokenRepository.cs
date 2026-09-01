50d817a7965ec8f6b6ba1c9d8a0605a73592e24d
mohammedhany4213-create <mohammedhany4213@gmail.com>
2026-09-01 18:49:20 +0300

    feat: make refresh token rotation atomic

---
+++ backend/src/coFind.Application/Interfaces/IRefreshTokenRepository.cs
@@ -0,0 +1,11 @@
+using coFind.Domain.Entities;
+
+namespace coFind.Application.Interfaces;
+
+public interface IRefreshTokenRepository
+{
+    Task<RefreshToken?> GetByHashAsync(string tokenHash, CancellationToken cancellationToken = default);
+    Task AddAsync(RefreshToken refreshToken, CancellationToken cancellationToken = default);
+    Task SaveChangesAsync(CancellationToken cancellationToken = default);
+    Task<bool> RotateAsync(RefreshToken currentToken, RefreshToken replacementToken, CancellationToken cancellationToken = default);
+}
