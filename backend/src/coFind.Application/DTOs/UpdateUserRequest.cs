2fcb0c295c0a000c29612cf5eb01b5d15056cd03
mohammedhany4213-create <mohammedhany4213@gmail.com>
2026-09-01 14:14:13 +0300

    fix: validate phone number on profile update

--- backend/src/coFind.Application/DTOs/UpdateUserRequest.cs
+++ backend/src/coFind.Application/DTOs/UpdateUserRequest.cs
@@ -3,6 +3,6 @@ using System.ComponentModel.DataAnnotations;
 namespace coFind.Application.DTOs;
 
 public record UpdateUserRequest(
-    [property: Required, MinLength(2), MaxLength(100)] string Name,
-    [property: Required, MaxLength(30)] string WhatsappNumber
+    [property: Required, MinLength(2), MaxLength(50)] string Name,
+    [property: Required, Phone, MaxLength(15)] string WhatsappNumber
 );
