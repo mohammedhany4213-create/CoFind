a93e2d41029a18d4eb2ba348c82c2020d9b2c777
mohammedhany4213-create <mohammedhany4213@gmail.com>
2026-09-02 18:06:23 +0300

    fix: apply validation metadata to update user parameters

--- backend/src/coFind.Application/DTOs/UpdateUserRequest.cs
+++ backend/src/coFind.Application/DTOs/UpdateUserRequest.cs
@@ -3,6 +3,6 @@ using System.ComponentModel.DataAnnotations;
 namespace coFind.Application.DTOs;
 
 public record UpdateUserRequest(
-    [property: Required, MinLength(2), MaxLength(50)] string Name,
-    [property: Required, Phone, MaxLength(15)] string WhatsappNumber
+    [Required, MinLength(2), MaxLength(50)] string Name,
+    [Required, Phone, MaxLength(15)] string WhatsappNumber
 );
